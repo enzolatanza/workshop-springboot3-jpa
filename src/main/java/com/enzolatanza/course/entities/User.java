7e6c26d505d64bed5c795adaa99966f2df454e00
Enzo Furlan Latanza <enzolatanza@gmail.com>
2023-11-16 23:40:49 -0300

    H2 database, test profile, JPA

--- src/main/java/com/enzolatanza/course/entities/User.java
+++ src/main/java/com/enzolatanza/course/entities/User.java
@@ -2,11 +2,21 @@ package com.enzolatanza.course.entities;
 
 import java.io.Serializable;
 import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 //implementar interface Serializable para que o o objeto possa ser transformado em cadeia de bytes
 //e assim trafegar na rede, gravar em arquivos
+@Entity 
+@Table(name = "tb_user")
 public class User implements Serializable{
 
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
 	private String email;
